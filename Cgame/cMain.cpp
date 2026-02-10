05fb9e9281c988ec2d03adc2c27e0dee32439469
lecture <ksw617@gmail.com>
2026-02-10 19:12:12 +0900

    Update cMain.cpp

--- Cgame/cMain.cpp
+++ Cgame/cMain.cpp
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <Windows.h> // WIN32 API
 
 int main()
 {
-	printf("Hello world\n");
+	//초기화
+
+	while (true)
+	{
+		system("cls"); // 화면 지워주고
+
+		//코드 작성
+		printf("Hello world");
+
+		Sleep(50); // 잠시 멈춤
+	}
+
 	return 0;
 }
