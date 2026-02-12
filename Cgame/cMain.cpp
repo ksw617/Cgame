329fa0c2a84a0f1ce94732ba496ddd181468ba39
lecture <45592229+ksw617@users.noreply.github.com>
2026-02-12 19:16:54 +0900

    Update cMain.cpp

--- Cgame/cMain.cpp
+++ Cgame/cMain.cpp
@@ -1,10 +1,6 @@
-#pragma region Header
 #include <stdio.h>
 #include <Windows.h>
-#include <time.h>
-#pragma endregion
 
-#pragma region Enum
 enum Color
 {
 	Black,
@@ -25,59 +21,27 @@ enum Color
 	White,
 };
 
-enum Dir
-{
-	Left,
-	Right,
-	Up,
-	Down
-};
-
-#pragma endregion
-
-#pragma region ±¸Á¶Ã¼
-
-struct Obj
-{
-	int x;
-	int y;
-	Color color;
-	const char* shape;
-
-};
-#pragma endregion
-
-#pragma region WIN_API
-void SetPosition(int x, int y);
-void SetTextColor(Color color);
 void HideCursor();
-#pragma endregion
+void SetTextColor(Color color);
+void SetPosition(int x, int y);
 
-#pragma region GAME
-
-Obj* player;
-Obj* enemy;
-int heart;
-
-void Init();
-void Progress();
-void Render();
-
-#pragma endregion
 
 
 int main()
 {
-
+	int x = 10;
+	int y = 10;
+	Color color = Yellow;
+	const char* shape = "Hello world";
 	HideCursor();
-	Init();
 
 	while (true)
 	{
 		system("cls");
 
-		Progress();
-		Render();
+		SetTextColor(color);
+		SetPosition(x, y);
+		printf(shape);
 
 		Sleep(50);
 	}
@@ -85,7 +49,6 @@ int main()
 	return 0;
 } 
 
-#pragma region WIN_API
 void SetPosition(int x, int y)
 {
 	COORD pos;
@@ -107,92 +70,3 @@ void HideCursor()
 	info.dwSize = 1;
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
 }
-
-#pragma endregion
-
-#pragma region GAME
-void Init()
-{
-	heart = 3;
-	srand((unsigned)time(NULL));
-
-	player = (Obj*)malloc(sizeof(Obj));
-	player->x = 10;
-	player->y = 10;
-	player->color = Blue;
-	player->shape = "¡á";
-
-	enemy = (Obj*)malloc(sizeof(Obj));
-	enemy->x = rand() % 30;
-	enemy->y = rand() % 30 + 1;
-	enemy->color = Red;
-	enemy->shape = "¢¾";
-}
-void Progress()
-{
-	if (GetAsyncKeyState(VK_LEFT))
-	{
-		player->x--;
-	}
-	if (GetAsyncKeyState(VK_RIGHT))
-	{
-		player->x++;
-	}
-
-	if (GetAsyncKeyState(VK_UP))
-	{
-		player->y--;
-	}
-
-	if (GetAsyncKeyState(VK_DOWN))
-	{
-		player->y++;
-	}
-
-	if (player->x == enemy->x && player->y == enemy->y)
-	{
-		enemy->x = rand() % 30;
-		enemy->y = rand() % 30 + 1;
-		heart++;
-	}
-
-	Dir randDir = (Dir)(rand() % 4);
-
-	switch (randDir)
-	{
-	case Left:
-		enemy->x--;
-		break;
-	case Right:
-		enemy->x++;
-		break;
-	case Up:
-		enemy->y--;
-		break;
-	case Down:
-		enemy->y++;
-		break;
-	default:
-		break;
-	}
-
-}
-void Render()
-{
-	SetPosition(player->x, player->y);
-	SetTextColor(player->color);
-	printf(player->shape);
-
-	SetPosition(enemy->x, enemy->y);
-	SetTextColor(enemy->color);
-	printf(enemy->shape);
-
-	for (int i = 0; i < heart; i++)
-	{
-		SetPosition(i, 0);
-		SetTextColor(Red);
-		printf("¢¾");
-	}
-}
-#pragma endregion
-
