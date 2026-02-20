7b783e09ca35744fea3887833fddd652c325d6b5
lecture <45592229+ksw617@users.noreply.github.com>
2026-02-20 19:02:39 +0900

    Update cMain.cpp

--- Cgame/cMain.cpp
+++ Cgame/cMain.cpp
@@ -72,139 +72,168 @@ int map[MAP_HEIGHT][MAP_WIDTH] =
 
 #pragma endregion
 
+#pragma region GAME
+Obj* player = nullptr;
+Obj* enemy = nullptr;
+void Init(); // 초기화
+void Render(); // 화면에 띄우는 용도
+void Progress(); // 게임 진행 요소들
+void Release();	// 동적할당된 메모리 해제
+
+
+#pragma endregion
+
+
 int main()
 {
-	Obj* player = (Obj*)malloc(sizeof(Obj));
-	player->x = 10;
-	player->y = 10;
-	player->color = Yellow;
-	player->shape = "옷";
-
-	Obj* enemy = (Obj*)malloc(sizeof(Obj));
-
-	//1번
-	do
-	{
-		enemy->x = rand() % 18 + 1;
-		enemy->y = rand() % 18 + 1;
-
-	} while ((map[enemy->y][enemy->x] == 1);
-
-	//2번
-	enemy->x = rand() % 18 + 1;
-	enemy->y = rand() % 18 + 1;
-	while (map[enemy->y][enemy->x] == 1)
-	{
-		enemy->x = rand() % 18 + 1;
-		enemy->y = rand() % 18 + 1;
-	}
-
-	//3번
-	enemy->x = rand() % 18 + 1;
-	enemy->y = rand() % 18 + 1;
-	while (true)
-	{
-		enemy->x = rand() % 18 + 1;
-		enemy->y = rand() % 18 + 1;
-		if (map[enemy->y][enemy->x] != 1)
-		{
-			break;
-		}
-	}
-
-
-	enemy->color = Red;
-	enemy->shape = "봇";
+	
 	HideCursor();
+	Init();
 
 	while (true)
 	{
 		system("cls");
 
-		for (int y = 0; y < MAP_HEIGHT; y++)
-		{
-			for (int x = 0; x < MAP_WIDTH; x++)
-			{
-				switch (map[y][x])
-				{
-				case 0:
-					break;
-				case 1:
-					SetTextColor(White);
-					SetPosition(x, y);
-					printf("■");
-					break;
-				case 2:
-					SetTextColor(Red);
-					SetPosition(x, y);
-					printf("♥");
-					break;
-				default:
-					break;
-				}
-			}
-		}
+		Progress();
+		Render();
 
-		if (GetAsyncKeyState(VK_LEFT))
-		{
-			player->x--;
-			if (map[player->y][player->x] == 1)
-			{
-				player->x++;
-			}
-		}
-
-
-		if (GetAsyncKeyState(VK_RIGHT))
-		{
-			player->x++;
-			if (map[player->y][player->x] == 1)
-			{
-				player->x--;
-			}
-		}
-
-
-		if (GetAsyncKeyState(VK_UP))
-		{
-			player->y--;
-			if (map[player->y][player->x] == 1)
-			{
-				player->y++;
-			}
-		}
-
-
-		if (GetAsyncKeyState(VK_DOWN))
-		{
-			player->y++;
-			if (map[player->y][player->x] == 1)
-			{
-				player->y--;
-			}
-		}
-
-		if (map[player->y][player->x] == 2)
-		{
-			map[player->y][player->x] = 0;
-		}
-
-		SetTextColor(player->color);
-		SetPosition(player->x, player->y);
-		printf(player->shape);
-
-		SetTextColor(enemy->color);
-		SetPosition(enemy->x, enemy->y);
-		printf(enemy->shape);
 	
 		Sleep(50);
 	}
 
-	free(player);
-	player = nullptr;
+	Release();
 
 	return 0;
 }
+
+#pragma region GAME
+#pragma region Init
+void Init()
+{
+	player = (Obj*)malloc(sizeof(Obj));
+	player->x = 10;
+	player->y = 10;
+	player->color = Yellow;
+	player->shape = "옷";
+
+	enemy = (Obj*)malloc(sizeof(Obj));
+	do
+	{
+		enemy->x = rand() % 18 + 1;
+		enemy->y = rand() % 18 + 1;
+
+	} while (map[enemy->y][enemy->x] == 1);
+
+	enemy->color = Red;
+	enemy->shape = "봇";
+}
+#pragma endregion
+#pragma region Render
+void Render()
+{
+	for (int y = 0; y < MAP_HEIGHT; y++)
+	{
+		for (int x = 0; x < MAP_WIDTH; x++)
+		{
+			switch (map[y][x])
+			{
+			case 0:
+				break;
+			case 1:
+				SetTextColor(White);
+				SetPosition(x, y);
+				printf("■");
+				break;
+			case 2:
+				SetTextColor(Red);
+				SetPosition(x, y);
+				printf("♥");
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	SetTextColor(player->color);
+	SetPosition(player->x, player->y);
+	printf(player->shape);
+
+	SetTextColor(enemy->color);
+	SetPosition(enemy->x, enemy->y);
+	printf(enemy->shape);
+}
+#pragma endregion
+#pragma region Progress
+void Progress()
+{
+	if (GetAsyncKeyState(VK_LEFT))
+	{
+		player->x--;
+		if (map[player->y][player->x] == 1)
+		{
+			player->x++;
+		}
+	}
+
+
+	if (GetAsyncKeyState(VK_RIGHT))
+	{
+		player->x++;
+		if (map[player->y][player->x] == 1)
+		{
+			player->x--;
+		}
+	}
+
+
+	if (GetAsyncKeyState(VK_UP))
+	{
+		player->y--;
+		if (map[player->y][player->x] == 1)
+		{
+			player->y++;
+		}
+	}
+
+
+	if (GetAsyncKeyState(VK_DOWN))
+	{
+		player->y++;
+		if (map[player->y][player->x] == 1)
+		{
+			player->y--;
+		}
+	}
+
+	if (map[player->y][player->x] == 2)
+	{
+		map[player->y][player->x] = 0;
+	}
+}
+#pragma endregion
+
+#pragma region Release
+void Release()
+{
+	if (player != nullptr)
+	{
+		free(player);
+		player = nullptr;
+	}
+
+	if (enemy != nullptr)
+	{
+		free(enemy);
+		enemy = nullptr;
+	}
+
+}
+#pragma endregion
+
+
+#pragma endregion
+
 
 #pragma region WIN_API
 
