cfcdb05349c326f56af4dcfd409fbea9266e61ac
lecture <45592229+ksw617@users.noreply.github.com>
2026-02-23 18:05:57 +0900

    Update cMain.cpp

--- Cgame/cMain.cpp
+++ Cgame/cMain.cpp
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <Windows.h>
 
-#pragma region Define
-
-#define MAP_WIDTH 20
-#define MAP_HEIGHT 20
-
-#pragma endregion
 #pragma region Enum
 enum Color
 {
@@ -28,318 +22,37 @@ enum Color
 	White,
 };
 
-enum DIR
-{
-	LEFT,
-	RIGHT,
-	UP,
-	DOWN,
-};
 #pragma endregion
-#pragma region Struct
-struct Obj
-{
-	int hp;
-	int x;
-	int y;
-	Color color;
-	const char* shape;
-};
-#pragma endregion
-
 #pragma region WIN_API
 void HideCursor();
 void SetTextColor(Color color);
 void SetPosition(int x, int y);
 #pragma endregion
 
-#pragma region MAP
-int map[MAP_HEIGHT][MAP_WIDTH] = {};
-int maps[2][MAP_HEIGHT][MAP_WIDTH] =
-{
-	{	
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-		{1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1},
-		{1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,1},
-		{1,0,0,0,1,0,0,0,1,1,1,0,1,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,1},
-		{1,0,0,0,1,1,1,0,1,0,0,0,1,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,2,0,1},
-		{1,0,1,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,1},
-		{1,0,1,1,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1},
-		{1,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,1},
-		{1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1},
-		{1,1,1,1,0,0,2,0,1,0,0,0,1,0,0,1,1,0,0,1},
-		{1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,1},
-		{1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,1},
-		{1,0,1,1,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,1,0,0,0,1,0,1,0,0,1,1,1,0,1,0,0,0,1},
-		{1,0,1,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,0,1},
-		{1,0,1,0,0,0,1,0,0,0,0,0,0,2,0,1,0,0,0,1},
-		{1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	},
-
-	{
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	}
-
-
-};
-
-#pragma endregion
-
-#pragma region GAME
-Obj* player = nullptr;
-Obj* enemy = nullptr;
-void Init(); // 초기화
-void Render(); // 화면에 띄우는 용도
-void Progress(); // 게임 진행 요소들
-void Release();	// 동적할당된 메모리 해제
-
-
-#pragma endregion
-
+void Logo();
+void Menu();
+void Stage();
 
 int main()
 {
 	
 	HideCursor();
-	Init();
+
 
 	while (true)
 	{
 		system("cls");
 
-		Progress();
-		Render();
-
+		SetTextColor(White);
+		SetPosition(10, 10);
+		printf("Hello world");
 	
 		Sleep(50);
 	}
 
-	Release();
 
 	return 0;
 }
-
-#pragma region GAME
-#pragma region Init
-void Init()
-{
-	for (int y = 0; y < MAP_HEIGHT; y++)
-	{
-		for (int x = 0; x < MAP_WIDTH; x++)
-		{
-			map[y][x] = maps[0][y][x];
-		}
-	}
-
-	player = (Obj*)malloc(sizeof(Obj));
-	player->hp = 3;
-	player->x = 10;
-	player->y = 10;
-	player->color = Yellow;
-	player->shape = "옷";
-
-	enemy = (Obj*)malloc(sizeof(Obj));
-	do
-	{
-		enemy->x = rand() % 18 + 1;
-		enemy->y = rand() % 18 + 1;
-
-	} while (map[enemy->y][enemy->x] == 1);
-
-	enemy->color = Red;
-	enemy->shape = "봇";
-}
-#pragma endregion
-#pragma region Render
-void Render()
-{
-	for (int y = 0; y < MAP_HEIGHT; y++)
-	{
-		for (int x = 0; x < MAP_WIDTH; x++)
-		{
-			switch (map[y][x])
-			{
-			case 0:
-				break;
-			case 1:
-				SetTextColor(White);
-				SetPosition(x, y);
-				printf("■");
-				break;
-			case 2:
-				SetTextColor(Red);
-				SetPosition(x, y);
-				printf("♥");
-				break;
-			default:
-				break;
-			}
-		}
-	}
-
-	for (int i = 0; i < player->hp; i++)
-	{
-		SetTextColor(Red);
-		SetPosition(20 + i, 0);
-		printf("♥");
-	}
-
-	SetTextColor(player->color);
-	SetPosition(player->x, player->y);
-	printf(player->shape);
-
-	SetTextColor(enemy->color);
-	SetPosition(enemy->x, enemy->y);
-	printf(enemy->shape);
-}
-#pragma endregion
-#pragma region Progress
-void Progress()
-{
-	if (GetAsyncKeyState(VK_LEFT))
-	{
-		player->x--;
-		if (map[player->y][player->x] == 1)
-		{
-			player->x++;
-		}
-	}
-
-
-	if (GetAsyncKeyState(VK_RIGHT))
-	{
-		player->x++;
-		if (map[player->y][player->x] == 1)
-		{
-			player->x--;
-		}
-	}
-
-
-	if (GetAsyncKeyState(VK_UP))
-	{
-		player->y--;
-		if (map[player->y][player->x] == 1)
-		{
-			player->y++;
-		}
-	}
-
-
-	if (GetAsyncKeyState(VK_DOWN))
-	{
-		player->y++;
-		if (map[player->y][player->x] == 1)
-		{
-			player->y--;
-		}
-	}
-
-	DIR dir = (DIR)(rand() % 4);
-
-	switch (dir)	
-	{
-	case LEFT:
-		enemy->x--;
-		break;
-	case RIGHT:
-		enemy->x++;
-		break;
-	case UP:
-		enemy->y--;
-		break;
-	case DOWN:
-		enemy->y++;
-		break;
-	default:
-		break;
-	}
-
-	if (map[enemy->y][enemy->x] == 1)
-	{
-		switch (dir)
-		{
-		case LEFT:
-			enemy->x++;
-			break;
-		case RIGHT:
-			enemy->x--;
-			break;
-		case UP:
-			enemy->y++;
-			break;
-		case DOWN:
-			enemy->y--;
-			break;
-		default:
-			break;
-		}
-	}
-
-	if (map[player->y][player->x] == 2)
-	{
-		map[player->y][player->x] = 0;
-		player->hp++;
-	}
-
-	if (player->x == enemy->x && player->y == enemy->y)
-	{
-		player->hp--;
-
-		if (player->hp <= 0)
-		{
-			exit(true);
-		}
-	}
-
-
-
-}
-#pragma endregion
-
-#pragma region Release
-void Release()
-{
-	if (player != nullptr)
-	{
-		free(player);
-		player = nullptr;
-	}
-
-	if (enemy != nullptr)
-	{
-		free(enemy);
-		enemy = nullptr;
-	}
-
-}
-#pragma endregion
-
-
-#pragma endregion
 
 
 #pragma region WIN_API
